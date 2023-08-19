a5b841c8b1ef1845ee21410c4f2426c1ac71c09e
0xKubitus <0xKubitus@gmail.com>
2023-08-19 23:36:15 +0200

    added: button to connect wallet (not implemented yet)

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -7,12 +7,17 @@ export default function Header() {
     <header className={styles.header_main}>
       <a href="#Home">
         <Image
+          className={styles.headerLogo}
           src="https://see.fontimg.com/api/renderfont4/L3wZ/eyJyIjoiZnMiLCJoIjoyNiwidyI6MTAwMCwiZnMiOjI2LCJmZ2MiOiIjMDAwMDAwIiwiYmdjIjoiI0ZGRkZGRiIsInQiOjF9/Q0FJUk8gTE9UTyA/gyptienne-normal.png"
           alt="Egyptian fonts"
           width={120}
           height={24}
         />
       </a>
+
+      <div className={styles.connectBox}>
+        <button className={styles.connectBtn}>Connect Wallet</button>
+      </div>
     </header>
   );
 }
