ed056cab8d16e4badab58898e34955292a6d9c1e
0xKubitus <0xKubitus@gmail.com>
2023-09-08 01:32:18 +0200

    balance displayed in the dashboard, let's display each tickets owned by the user now

--- src/components/nft/gallery.jsx
+++ src/components/nft/gallery.jsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { useContractRead } from "@starknet-react/core";
+import { useState, useEffect } from "react";
+
+import { useContractRead, useWaitForTransaction } from "@starknet-react/core";
 import { json } from "starknet";
 
 import ticketNft from "@/assets/abis/abi_TicketsHandler_v0.4.json"; // make sure to import the latest TicketsHandler version's abi
@@ -9,15 +11,47 @@ import environment from "environment";
 // Compiled ABI for my 'Tickets' contract
 const compiledTicketNft = json.parse(JSON.stringify(ticketNft));
 
-export default function Gallery({ balance, address }) {
-  const ownedTickets = [];
+// Fetching balance of an account
+const TicketsDetails = ({ account, burnHash }) => {
+  // #########################################################################
+  // TICKETS BALANCE
 
-  // console.log(typeof balance); // to be deleted
+  // Internal state representing the current balance of the account
+  const [balance, setBalance] = useState(0);
 
+  // use useWaitForTransaction hook to wait for the burn transaction to be accepted on L2 -> Use to refresh the balance after burn
+  const { data } = useWaitForTransaction({ hash: burnHash, watch: true });
+
+  // use useContractRead hook to fetch the balance of the account
+  const { data: tokenBalanceData, refetch } = useContractRead({
+    address: environment.nftAddress,
+    abi: compiledTicketNft,
+    functionName: "balanceOf",
+    args: [account.address],
+  });
+
+  // Set the internal state balance when the tokenBalanceData is fetched
+  useEffect(() => {
+    if (tokenBalanceData) {
+      setBalance(tokenBalanceData.balance.low);
+    }
+  }, [tokenBalanceData]);
+
+  // Refresh the balance when the burn transaction is accepted on L2
+  useEffect(() => {
+    if (data && data.status === "ACCEPTED_ON_L2") refetch();
+  }, [refetch, data]);
+
+  // #########################################################################
+
+  return <p>You possess: {balance.toString()} tickets</p>;
+};
+
+export default function Gallery({ account }) {
   return (
     <div>
       <p>Gallery</p>
-      <p>balance = {balance.toString()}</p>
+      <TicketsDetails account={account} />
     </div>
   );
 }
