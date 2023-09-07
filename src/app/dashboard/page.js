6375ad0f260c27f5fc60765ebf7885cce8a591f3
0xKubitus <0xKubitus@gmail.com>
2023-09-07 09:23:48 +0200

    just a random save

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -11,63 +11,55 @@ import { json } from "starknet";
 
 import environment from "environment";
 import ticketNft from "@/assets/abis/abi_TicketsHandler_v0.4.json"; // make sure to import the latest TicketsHandler version's abi
-// import Gallery from "@/components/nft/gallery";
+import Gallery from "@/components/nft/gallery";
 
 export default function Dashboard() {
-  const { account } = useAccount();
+  const { address } = useAccount();
 
   // Compiled ABI for my 'Tickets' contract
   const compiledTicketNft = json.parse(JSON.stringify(ticketNft));
 
-  // Fetching balance of an account
-  const Balance = ({ account, mintHash }) => {
-    // Internal state representing the current balance of the account
-    const [balance, setBalance] = useState(0);
+  // Internal state representing the current balance of the account
+  const [balance, setBalance] = useState(0);
 
-    // use useWaitForTransaction hook to wait for the mint transaction to be accepted on L2 -> Use to refresh the balance after mint
-    const { data } = useWaitForTransaction({ hash: mintHash, watch: true });
+  // use useWaitForTransaction hook to wait for the mint transaction to be accepted on L2 -> Use to refresh the balance after mint
+  // const { data } = useWaitForTransaction({ hash: mintHash, watch: true });
 
-    // use useContractRead hook to fetch the balance of the account
-    const { data: tokenBalanceData, refetch } = useContractRead({
-      address: environment.nftAddress,
-      abi: compiledTicketNft,
-      functionName: "balanceOf",
-      args: [account.address],
-    });
+  // use useContractRead hook to fetch the balance of the account
+  const { data: tokenBalanceData, refetch } = useContractRead({
+    address: environment.nftAddress,
+    abi: compiledTicketNft,
+    functionName: "balanceOf",
+    args: [address],
+  });
 
-    // Set the internal state balance when the tokenBalanceData is fetched
-    useEffect(() => {
-      if (tokenBalanceData) {
-        setBalance(tokenBalanceData.balance.low);
-      }
-    }, [tokenBalanceData]);
+  // Set the internal state balance when the tokenBalanceData is fetched
+  useEffect(() => {
+    if (tokenBalanceData) {
+      setBalance(tokenBalanceData.balance.low);
+    }
+  }, [tokenBalanceData]);
 
-    // Refresh the balance when the mint transaction is accepted on L2
-    useEffect(() => {
-      if (data && data.status === "ACCEPTED_ON_L2") refetch();
-    }, [refetch, data]);
-  };
+  // Refresh the balance when the mint transaction is accepted on L2
+  // useEffect(() => {
+  //   if (data && data.status === "ACCEPTED_ON_L2") refetch();
+  // }, [refetch, data]);
 
   return (
     <div>
       <h1>Dashboard</h1>
-      {account ? (
+      {address ? (
         <>
           <p>
             Tickets Balance:
-            {
-              <Balance
-                // burnHash={txDataBurn?.transaction_hash}
-                account={account}
-              />
-            }
+            {<span>{balance.toString()}</span>}
           </p>
         </>
       ) : (
         "Please connect your Starknet wallet"
       )}
       <h2>TICKETS IN YOUR POSSESSION</h2>
-      {/* <Gallery /> */}
+      <Gallery balance={balance} address={address} />
       {/* balance={balance} */}
     </div>
   );
--- src/components/nft/gallery.jsx
+++ src/components/nft/gallery.jsx
@@ -1,4 +1,23 @@
-export default function Gallery() {
+"use client";
+
+import { useContractRead } from "@starknet-react/core";
+import { json } from "starknet";
+
+import ticketNft from "@/assets/abis/abi_TicketsHandler_v0.4.json"; // make sure to import the latest TicketsHandler version's abi
+import environment from "environment";
+
+// Compiled ABI for my 'Tickets' contract
+const compiledTicketNft = json.parse(JSON.stringify(ticketNft));
+
+export default function Gallery({ balance, address }) {
   const ownedTickets = [];
-  return <div>gallery</div>;
+
+  // console.log(typeof balance); // to be deleted
+
+  return (
+    <div>
+      <p>Gallery</p>
+      <p>balance = {balance.toString()}</p>
+    </div>
+  );
 }
