ceb5a81fcc39fdec4135d491da982983cab68f3f
0xKubitus <0xKubitus@gmail.com>
2023-09-07 04:47:42 +0200

    dashboard displays user tickets' balance

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,8 +1,37 @@
-export default function Dashboard() {
+"use client";
+
+import { useAccount } from "@starknet-react/core";
+import { json } from "starknet";
+
+import Balance from "@/components/tickets-balance/tickets-balance";
+
+export default function Dashboard({ ...props }) {
+  const { account } = useAccount();
+
   return (
     <div>
-      <p>Dashboard</p>
-      <p>wesh</p>
+      <h1>Dashboard</h1>
+
+      {account ? (
+        <>
+          <p>
+            Tickets Balance:{" "}
+            {
+              <Balance
+                // mintHash={txDataMint?.transaction_hash}
+                account={account}
+              />
+            }
+          </p>
+        </>
+      ) : (
+        "Please connect your Starknet wallet"
+      )}
+
+      {/* <Balance
+        account={account}
+        // mintHash={txDataMint?.transaction_hash}
+      /> */}
     </div>
   );
 }
--- src/components/nft/mint-btn.jsx
+++ src/components/nft/mint-btn.jsx
@@ -1,125 +1,54 @@
-// I have to replace "ApproveAndMint" by just "Mint" throughout this file!
-
 "use client";
 
 import { useEffect, useState } from "react";
 
 import {
   useAccount,
-  useContractRead,
+  // useContractRead,
   useContractWrite,
   useTransactionManager,
-  useWaitForTransaction,
+  // useWaitForTransaction,
 } from "@starknet-react/core";
 import { json } from "starknet";
 
-// import ticketNft from "@/assets/abis/abi_Tickets_v0.3.2.json";
-import ticketNft from "@/assets/abis/abi_TicketsHandler_v0.4.json";
+import Balance from "@/components/tickets-balance/tickets-balance";
 import environment from "environment";
 
 import styles from "@/styles/mint-btn.module.css";
 
-// Compiled ABI for my 'Tickets' contract
-const compiledTicketNft = json.parse(JSON.stringify(ticketNft));
-
-// Fetching balance of an account
-const Balance = ({ account, mintHash }) => {
-  // Internal state representing the current balance of the account
-  const [balance, setBalance] = useState(0);
-
-  // use useWaitForTransaction hook to wait for the mint transaction to be accepted on L2 -> Use to refresh the balance after mint
-  const { data } = useWaitForTransaction({ hash: mintHash, watch: true });
-
-  // use useContractRead hook to fetch the balance of the account
-  const { data: tokenBalanceData, refetch } = useContractRead({
-    address: environment.nftAddress,
-    abi: compiledTicketNft,
-    functionName: "balanceOf",
-    args: [account.address],
-  });
-
-  // Set the internal state balance when the tokenBalanceData is fetched
-  useEffect(() => {
-    if (tokenBalanceData) {
-      setBalance(tokenBalanceData.balance.low);
-    }
-  }, [tokenBalanceData]);
-
-  // Refresh the balance when the mint transaction is accepted on L2
-  useEffect(() => {
-    if (data && data.status === "ACCEPTED_ON_L2") refetch();
-  }, [refetch, data]);
-
-  return <p>{balance.toString()}</p>;
-};
-
 export default function MintButton({ ...props }) {
   const { account } = useAccount();
   const { addTransaction } = useTransactionManager();
-  const bookKeeper = environment.bookKeeperAddress;
+  // const bookKeeper = environment.bookKeeperAddress;
 
-  //#############################################
-  // AVNU WORKSHOP IMPLEMENTATION (ISSUE = ANYONE CAN MINT FOR FREE IF THEY WANT)
-
-  // use useContractWrite hook to make a multicall: the mint function of my 'Tickets' contract && transfer some eth to the contract (multicall)
-  // const { data: txDataMintAndTransfer, write: writeMintAndTransfer } =
-  //   useContractWrite({
-  //     calls: [
-  //       {
-  //         contractAddress: environment.nftAddress,
-  //         entrypoint: "mint",
-  //         calldata: [account?.address || "0x0"],
-  //       },
-  //       {
-  //         contractAddress: environment.ethAddress,
-  //         entrypoint: "transfer",
-  //         calldata: [
-  //           "0x026a65e469699ab527fbe15819c1abe4ee10cfeeec4cb3015576cbef9fc5503d", // this is one of my Braavos accounts on testnet
-  //           1,
-  //           0,
-  //         ],
-  //       },
-  //     ],
-  //   });
-  //#############################################
-
-  // use useContractWrite hook to make a first transaction: "approve(ticket_price)" an ERC20 token to be spent by TicketsHandler contract
-  const { data: txDataApproveAndMint, write: writeApproveAndMint } =
-    useContractWrite({
-      calls: [
-        {
-          contractAddress: environment.nftAddress,
-          entrypoint: "mint",
-          calldata: [],
-        },
-      ],
-    });
+  // use useContractWrite hook to invoke the 'mint()' function from TicketsHandler contract
+  const { data: txDataMint, write: writeMint } = useContractWrite({
+    calls: [
+      {
+        contractAddress: environment.nftAddress,
+        entrypoint: "mint",
+        calldata: [],
+      },
+    ],
+  });
 
   // Add transaction to the manager once transaction is submitted
   useEffect(() => {
-    if (txDataApproveAndMint)
-      addTransaction({ hash: txDataApproveAndMint.transaction_hash });
-  }, [txDataApproveAndMint, addTransaction]);
+    if (txDataMint) addTransaction({ hash: txDataMint.transaction_hash });
+  }, [txDataMint, addTransaction]);
 
   return (
     <div>
       <div className={styles.userBalanceContainer}>
-        <p>Tickets Balance:</p>
+        <p>Your Tickets Balance:</p>
         {account ? (
-          <Balance
-            mintHash={txDataApproveAndMint?.transaction_hash}
-            account={account}
-          />
+          <Balance mintHash={txDataMint?.transaction_hash} account={account} />
         ) : (
-          "0"
+          "Please connect your Starknet wallet"
         )}
       </div>
 
-      <button
-        className={styles.buyBtn}
-        onClick={() => writeApproveAndMint()}
-        {...props}
-      >
+      <button className={styles.buyBtn} onClick={() => writeMint()} {...props}>
         Buy a Ticket for 0.001 ETH
       </button>
     </div>
